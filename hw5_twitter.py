7ac8493edfdd4ae9cdd4f2d1f53ff65c94148a12
Sam Lu <ulmas@umich.edu>
2018-02-10 13:25:18 -0500

    hallelujah heaven on high the tweets finally printed

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -63,10 +63,9 @@ requests.get(url, auth=auth)
 print('USER:', username)
 print('TWEETS ANALYZED:', num_tweets)
 
-baseURL = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-# compare to twitter example https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
+baseURL = 'https://api.twitter.com/1.1/statuses/user_timeline.json' # compare to twitter example https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
 p = {'screen_name':username, 'count':num_tweets} #screen_name is the actual username displayed, count specifies number of tweets to retrieve
-firstRequest = requests.get(baseURL, p)
+firstRequest = requests.get(baseURL, p, auth = auth)
 convertedRequest = json.loads(firstRequest.text)
 print(convertedRequest)
 
